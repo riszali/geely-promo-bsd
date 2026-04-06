a7cf5657661729d00916173eb86c7a210ad3d2f4
Rizki Ali Akhbar <rizkialiakhbar@gmail.com>
2026-04-07 01:16:38 +0700

    update no tlp

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -67,7 +67,7 @@
                     <div class="flex items-center gap-4 group">
                         <!-- Menambahkan flex-shrink-0 dan mengubah ukuran menjadi w-5 h-5 -->
                         <svg class="w-5 h-5 flex-shrink-0 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg>
-                        <p class="text-gray-400 text-[10px] tracking-[0.2em] font-semibold">0857720873354</p>
+                        <p class="text-gray-400 text-[10px] tracking-[0.2em] font-semibold">+6282246666904</p>
                     </div>
                 </div>
             </div>
